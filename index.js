76f1e695e1f85bc82e5d6dcd00faa270ef2e8000
ChristianMFlatiron <chrisrmonteverde@gmail.com>
2021-04-14 16:41:57 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun("bathe my dog")
+
+function mondayWork(act = "go to the office"){
+    return `This Monday, I will ${act}.`
+}
+mondayWork("work from home")
+
+function wrapAdjective(something = "*"){
+    return function inner(hype = "special"){
+        return `You are ${something}${hype}${something}!`
+    }
+}
